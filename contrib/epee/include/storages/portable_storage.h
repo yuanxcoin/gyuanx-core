73a341f514bec568ceeb8822b570d82492d5d179
Jason Rhinelander <jason@imaginary.ca>
2020-04-23 17:39:18 -0300

    No, epee, it's not ok

--- contrib/epee/include/storages/portable_storage.h
+++ contrib/epee/include/storages/portable_storage.h
@@ -80,7 +80,7 @@ namespace epee
       bool        delete_entry(const std::string& pentry_name, hsection hparent_section = nullptr);
 
       //-------------------------------------------------------------------------------
-      bool		store_to_binary(binarybuffer& target);
+      bool		store_to_binary(std::string& target);
       bool		load_from_binary(const epee::span<const uint8_t> target);
       bool		load_from_binary(const std::string& target) { return load_from_binary(epee::strspan<uint8_t>(target)); }
       template<class trace_policy>
@@ -132,7 +132,7 @@ namespace epee
     }
 
     inline
-    bool portable_storage::store_to_binary(binarybuffer& target)
+    bool portable_storage::store_to_binary(std::string& target)
     {
       TRY_ENTRY();
       std::stringstream ss;
