9a9ef745b7ec551402241e7f03cd090ef9ad3dd2
Jason Rhinelander <jason@imaginary.ca>
2020-06-02 22:58:26 -0300

    Remove unused call_sys_cmd function

--- contrib/epee/include/misc_os_dependent.h
+++ contrib/epee/include/misc_os_dependent.h
@@ -91,31 +91,6 @@ namespace misc_utils
         }
 
 
-        inline int call_sys_cmd(const std::string& cmd)
-	{      
-                std::cout << "# " << cmd << std::endl;
-
-		FILE * fp ;
-		//char tstCommand[] ="ls *";
-		char path[1000] = {0};
-#if !defined(__GNUC__) 
-		fp = _popen(cmd.c_str(), "r");
-#else
-		fp = popen(cmd.c_str(), "r");
-#endif
-		while ( fgets( path, 1000, fp ) != NULL )
-			std::cout << path;
-
-#if !defined(__GNUC__) 
-		_pclose(fp);
-#else
-		pclose(fp);
-#endif
-		return 0;
-
-	}
-
-
 	inline std::string get_thread_string_id()
 	{
 #if defined(_WIN32)
