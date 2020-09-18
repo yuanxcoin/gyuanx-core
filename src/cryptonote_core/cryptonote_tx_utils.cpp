077ca0ff4e33a6dc6384d198fa0893c4506fbb71
Jason Rhinelander <jason@imaginary.ca>
2020-09-18 19:03:58 -0300

    Update block rewards as per LRC-7

--- src/cryptonote_core/cryptonote_tx_utils.cpp
+++ src/cryptonote_core/cryptonote_tx_utils.cpp
@@ -133,8 +133,9 @@ namespace cryptonote
 
   uint64_t governance_reward_formula(uint64_t base_reward, uint8_t hf_version)
   {
-    return hf_version >= network_version_16_pulse ? FOUNDATION_REWARD_HF16 :
-           hf_version >= network_version_15_lns ? FOUNDATION_REWARD_HF15 :
+    return hf_version >= network_version_17         ? FOUNDATION_REWARD_HF17 :
+           hf_version >= network_version_16_pulse   ? FOUNDATION_REWARD_HF15 + BLOCKSWAP_LIQUIDITY_HF16 :
+           hf_version >= network_version_15_lns     ? FOUNDATION_REWARD_HF15 :
            base_reward / 20;
   }
 
@@ -197,7 +198,6 @@ namespace cryptonote
   uint64_t service_node_reward_formula(uint64_t base_reward, uint8_t hard_fork_version)
   {
     return
-      hard_fork_version >= network_version_16_pulse        ? SN_REWARD_HF16 :
       hard_fork_version >= network_version_15_lns          ? SN_REWARD_HF15 :
       hard_fork_version >= network_version_9_service_nodes ? base_reward / 2 : // 50% of base reward up until HF15's fixed payout
       0;
