d859605b11f162c22f636449f4cfc90b1b00335e
Doyle <doylet@protonmail.com>
2020-09-30 16:21:16 +1000

    HF16: Set min storage server version to 2.0.7

--- src/cryptonote_core/service_node_rules.h
+++ src/cryptonote_core/service_node_rules.h
@@ -211,7 +211,7 @@ namespace service_nodes {
   // blocks out of sync and sending something that it thinks is legit.
   constexpr uint64_t VOTE_OR_TX_VERIFY_HEIGHT_BUFFER    = 5;
 
-  constexpr std::array<int, 3> MIN_STORAGE_SERVER_VERSION{{2, 0, 0}};
+  constexpr std::array<int, 3> MIN_STORAGE_SERVER_VERSION{{2, 0, 7}};
   constexpr std::array<int, 3> MIN_LOKINET_VERSION{{0, 7, 0}};
 
   // The minimum accepted version number, broadcasted by Service Nodes via uptime proofs for each hardfork
