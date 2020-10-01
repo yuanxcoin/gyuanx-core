915e6109f6ae40b7a22eeca5f4e6e7562fd21c5e
Jason Rhinelander <jason@imaginary.ca>
2020-09-30 22:36:25 -0300

    Bump version to 8.1.0 and set HF16/17 fork times (#1295)

--- src/cryptonote_core/service_node_rules.h
+++ src/cryptonote_core/service_node_rules.h
@@ -222,7 +222,7 @@ namespace service_nodes {
   };
 
   constexpr proof_version MIN_UPTIME_PROOF_VERSIONS[] = {
-    {cryptonote::network_version_16_pulse,                {8,0,0}},
+    {cryptonote::network_version_16_pulse,                {8,1,0}},
     {cryptonote::network_version_15_lns,                  {7,1,2}},
     {cryptonote::network_version_14_blink,                {6,1,0}},
     {cryptonote::network_version_13_enforce_checkpoints,  {5,1,0}},
